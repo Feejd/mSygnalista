501d29d49eaf5fb6ec66da2f6921986769e641a2
Feejd <71599497+Feejd@users.noreply.github.com>
2026-09-21 10:47:34 +0200

    Create config.php
    plik konfiguracyjny bazy danych

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// Uwaga! To dane dla MAMP, zmień je dla innych serwerów
+return [
+    'host' => '127.0.0.1',
+    'port' => 8889,
+    'database' => 'msygnalista',
+    'username' => 'root',
+    'password' => 'root',
+];
